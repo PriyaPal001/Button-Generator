cade7fc1b207d78f88c4a2c4287e9e217c337f1c
PriyaPal001 <vu3t1s2021032@pvppcoe.ac.in>
2023-02-07 01:15:16 +0530

    changes made

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess'
 
-// server.hmr.overlay = false;
-// https://vitejs.dev/config/
+
 
 export default defineConfig({
-    // server: {
-    //     hmr: {
-    //         overlay: false,
-    //     },
-    // },
+    base: '/Button-Generator/',
     plugins: [svelte({
         preprocess: sveltePreprocess(),
 
